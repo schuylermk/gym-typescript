67abfc28205d93acdcd0ca0cb87d958946755054
schuylermk <schuyler.m.k@gmail.com>
2023-11-03 11:24:58 -0700

    Navbar progress

--- src/scenes/navbar/index.tsx
+++ src/scenes/navbar/index.tsx
@@ -1,14 +1,22 @@
 import { useState } from "react";
 import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/solid";
-import Logo from "@assets/Logo.png";
+import Logo from "@/assets/Logo.png";
+import { SelectedPage } from "@/shared/types";
+import useMediaQuery from "@/hooks/useMediaQuery";
+import ActionButton from "@/shared/ActionButton";
+import Link from "@/scenes/navbar/";
 
 type Props = {
-  selectedPage: string;
-  setSelectedPage: (value: string) => void;
+  isTopOfPage: boolean;
+  selectedPage: SelectedPage;
+  setSelectedPage: (value: SelectedPage) => void;
 }
 
-const Navbar = ({ selectedPage, setSelectedPage }: Props) => {
+const Navbar = ({ isTopOfPage, selectedPage, setSelectedPage }: Props) => {
   const flexBetween = "flex items-center justify-between";
+  const [isMenuToggled, setIsMenuToggled] = useState<boolean>(false);
+  const isAboveMediumScreens  = useMediaQuery("(min-width: 1060px)");
+  const navbarBackground = isTopOfPage ? "" : "bg-primary-100 drop-shadow";
   
   return (
     <nav>
